540159be8bf4b9d983f6c07efc9006e81a7065a0
Nick Tyler <nicholas.s.tyler.4@gmail.com>
2026-05-01 23:38:41 +0000

    Add user to start

--- htcondor_configs/start.sh
+++ htcondor_configs/start.sh
@@ -10,9 +10,11 @@ mkdir -p /scratch/spool
 
 exec condor_master -f &
 
+USER=tylren
+USERNAME=tylern
+
 # shellcheck source=/dev/null
 source /app/.venv/bin/activate
-
 exec gunicorn --log-level debug -w 1 --threads 16 -k uvicorn.workers.UvicornWorker htcondor_rest.app:app --bind 0.0.0.0:8008 &
 
 wait
